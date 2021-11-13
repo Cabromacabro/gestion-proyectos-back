604a8ebc4cb8b5d843cce24ec91929d5c441e9d7
Cabromacabro <oscarpedraza1@hotmail.com>
2021-11-12 21:26:40 -0500

    nuevos ajustes

--- db/db.ts
+++ db/db.ts
@@ -3,15 +3,6 @@ import { connect } from 'mongoose';
 
 const conectarBD = async () => {
   return await connect(
-<<<<<<< HEAD
-    'mongodb+srv://admin:Admin666@gestionproyectossowace.gdbfi.mongodb.net/gestionProyectos?retryWrites=true&w=majority'
-  )
-    .then(() => {
-      console.log('DB Connected');
-      })
-    .catch ((e) => {
-      console.error("Error conectando la DB", e);
-=======
     'mongodb+srv://admin:AdminProyectos@gestionproyectosmisiont.1zmu5.mongodb.net/GestionProyectos?retryWrites=true&w=majority'
   )
     .then(() => {
@@ -19,7 +10,6 @@ const conectarBD = async () => {
     })
     .catch((e) => {
       console.error('Error conectando a la bd', e);
->>>>>>> origin/explicaciones_mongoose
     });
 };
 
--- index.ts
+++ index.ts
@@ -8,20 +8,6 @@ import { ObjectiveModel } from './models/objective';
 const main = async () => {
   await conectarBD();
 
-<<<<<<< HEAD
-// Customer.insertMany([
-//   {
-//     name: 'Oscar',
-//     email: 'inhelltorment@gmail.com',
-//   },
-//   {
-//     name: 'Alex',
-//     email: 'oscarpedrazamartinez78@gmail.com',
-//   },
-// ])
-//   .then((c) => {
-//     console.log(c);
-=======
   // const objet = await ObjectiveModel.create({
   //   descripcion: 'Este es el objetivo especifico',
   //   tipo: Enum_TipoObjetivo.especifico,
@@ -60,32 +46,13 @@ main();
 // })
 //   .then((u) => {
 //     console.log('usuario creado', u);
->>>>>>> origin/explicaciones_mongoose
 //   })
 //   .catch((e) => {
 //     console.error('Error creando el usuario', e);
 //   });
 
-<<<<<<< HEAD
-// const customers = await Customer.find().then((c) => {
-//   return c;
-// });
-// Order.create({
-//   total: 1500,
-//   customer_id: customers[0]._id,
-// });
-
-// UserModel.create({
-//   name: 'Oscar',
-//   lastName: 'Pedraza,
-//   document: '11201445',
-//   email: 'inhelltorment@gmail.com',
-//   role: Enums.Enum_UserRole.estudiante,
-// })
-=======
 // OBTENER LOS USUARIOS
 // await UserModel.find()
->>>>>>> origin/explicaciones_mongoose
 //   .then((u) => {
 //     console.log('usuarios', u);
 //   })
@@ -93,21 +60,6 @@ main();
 //     console.error('error obteniendo los usuarios', e);
 //   });
 
-<<<<<<< HEAD
-/*ProjectModel.create({
-  name: 'Test',
-  budget: 120,
-  startDate: Date.now(),
-  finishDate: new Date('2022/11/10'),
-  leader: '6187d3a20a1d2fc06ea9b1f0',
-})
-  .then((p) => {
-    console.log('project', p);
-  })
-  .catch((e) => {
-    console.error(e);
-  });*/
-=======
 // OBTENER UN SOLO USUARIO
 // await UserModel.findOne({ identificacion: '16546' })
 //   .then((u) => {
@@ -116,7 +68,6 @@ main();
 //   .catch((e) => {
 //     console.error(e);
 //   });
->>>>>>> origin/explicaciones_mongoose
 
 // EDITAR UN USUARIO
 // await UserModel.findOneAndUpdate(
@@ -133,18 +84,6 @@ main();
 //     console.error('Error actualizando', e);
 //   });
 
-<<<<<<< HEAD
-const main = async () => {
-  await connectDB();
-
-  // order = await Order.find({ customer_id: '6186629a2dde6bb7f645aeaf' });
-  // console.log(order);
-};
-
-
-
-main();
-=======
 // ELIMINAR UN USUARIO
 // await UserModel.findOneAndDelete({ correo: 'dsl@cc.com' })
 //   .then((u) => {
@@ -153,4 +92,3 @@ main();
 //   .catch((e) => {
 //     console.error(e);
 //   });
->>>>>>> origin/explicaciones_mongoose
