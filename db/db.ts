b957903debf17971f02fb5114e148ec08ebb272c
Cabromacabro <oscarpedraza1@hotmail.com>
2021-11-12 21:45:57 -0500

    Ajuste url BD

--- db/db.ts
+++ db/db.ts
@@ -3,7 +3,7 @@ import { connect } from 'mongoose';
 
 const conectarBD = async () => {
   return await connect(
-    'mongodb+srv://admin:AdminProyectos@gestionproyectosmisiont.1zmu5.mongodb.net/GestionProyectos?retryWrites=true&w=majority'
+    'mongodb+srv://admin:Admin666@gestionproyectossowace.gdbfi.mongodb.net/gestionProyectos?retryWrites=true&w=majority'
   )
     .then(() => {
       console.log('Conexion exitosa');
